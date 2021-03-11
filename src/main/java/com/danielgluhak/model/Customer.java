5ab8395578975a4e4d072ac44c237121d4991fca
MojProfil <gluhak.daniel@users.noreply.github.com>
2021-03-11 22:50:29 +0100

    Annotation @notnull, @notempty & @size added

--- src/main/java/com/danielgluhak/model/Customer.java
+++ src/main/java/com/danielgluhak/model/Customer.java
@@ -3,6 +3,9 @@ package com.danielgluhak.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * 
@@ -11,13 +14,21 @@ import javax.persistence.Entity;
 @Entity
 public class Customer extends EntityDefault{
     
-//    @Column(name = "First name", columnDefinition = "VARCHAR(50) NOT NULL")
+    @NotNull(message = "Name must be set (null)")
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min=2, max=50, message = "Name length must be between 2 and 50 characters long")
     private String name;
-//    @Column(name = "Last name", columnDefinition = "VARCHAR(50) NOT NULL")
+    
+    @NotNull(message = "Last name must be set (null)")
+    @NotEmpty(message = "Last name cannot be empty")
+    @Size(min=2, max=50, message = "Last name length must be between 2 and 50 characters long")
     private String lastName;
-//    @Column(name = "Phone number", columnDefinition = "VARCHAR(100) NOT NULL")
+    
+    @NotNull(message = "Contact number must be set (null)")
+    @NotEmpty(message = "Please enter contact number")
     private String contact;
 
+    
     public String getName() {
         return name;
     }
