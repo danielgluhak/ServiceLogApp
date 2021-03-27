02e2cd8ed312908246736d863c0b5b39650dfbca
MojProfil <gluhakdaniel@gmail.com>
2021-03-27 19:23:20 +0100

    Created new entity

---
+++ src/main/java/com/danielgluhak/model/Operator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.danielgluhak.model;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Operator {
+ 
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
