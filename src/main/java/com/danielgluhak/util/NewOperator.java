237f2bd972f5717c5eb85ea4cd9e2eee3ce4b101
MojProfil <gluhakdaniel@gmail.com>
2021-03-28 17:42:12 +0200

    Update NewOperator

--- src/main/java/com/danielgluhak/util/NewOperator.java
+++ src/main/java/com/danielgluhak/util/NewOperator.java
@@ -5,10 +5,34 @@
  */
 package com.danielgluhak.util;
 
+import com.danielgluhak.controller.ControllerOperator;
+import com.danielgluhak.model.Operator;
+import org.mindrot.jbcrypt.BCrypt;
+
 /**
  *
  * @author Daniel
  */
 public class NewOperator {
+
+    public static void main(String[] args) {
+        Operator o1 = new Operator();
+        o1.setFirstName("Daniel");
+        o1.setLastName("Gluhak");
+        o1.setPassword(BCrypt.hashpw("defaultPass012", BCrypt.gensalt()));
+        
+        ControllerOperator co = new ControllerOperator();
+        co.setEntityDefault(o1);
+        
+        try {
+            co.create();
+        } catch (ExceptionServiceLog e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
+    
+    
     
 }
