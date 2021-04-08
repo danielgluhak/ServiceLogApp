37c058721aae962186393258db8f2b600acac4b8
MojProfil <gluhakdaniel@gmail.com>
2021-04-08 22:36:35 +0200

    ControllerService class major updates

--- src/main/java/com/danielgluhak/controller/ControllerServices.java
+++ src/main/java/com/danielgluhak/controller/ControllerServices.java
@@ -7,6 +7,7 @@ package com.danielgluhak.controller;
 
 import com.danielgluhak.model.Services;
 import com.danielgluhak.util.ExceptionServiceLog;
+import java.math.BigDecimal;
 import java.util.List;
 import org.hibernate.CacheMode;
 
@@ -29,15 +30,44 @@ public class ControllerServices extends Controller<Services> {
                 .setParameter("condition", "%" + condition + "%")
                 .list();
     }
+    
+    protected void nameSet()throws ExceptionServiceLog {
+        if(EntityDefault.getProductName().isBlank() || 
+            EntityDefault.getProductName().isEmpty()){
+            throw new ExceptionServiceLog("Please enter service name.");
+        }
+    }
+    protected void length()throws ExceptionServiceLog {
+        if(EntityDefault.getProductName().length()<3 ||
+                EntityDefault.getArticleCode().length()<3) {
+            throw new ExceptionServiceLog("Service name and service code \n must contain 3 characters or more.");
+        }
+                
+    }
+    protected void codeSet()throws ExceptionServiceLog {
+        if(EntityDefault.getArticleCode().isBlank() || 
+            EntityDefault.getArticleCode().isEmpty()) {
+            throw new ExceptionServiceLog("Please enter service code.");
+        }
+    }
+    protected void priceSet()throws ExceptionServiceLog {
+        if(EntityDefault.getPrice()==null &&
+                EntityDefault.getPrice().compareTo(BigDecimal.ZERO)<1) {
+            throw new ExceptionServiceLog("Please enter price. \n Price value must be greater than 1€.");
+        }
+    }
 
     @Override
     protected void controlCreate() throws ExceptionServiceLog {
-        
+        nameSet();
+        codeSet();
+        priceSet();
+        length();
     }
 
     @Override
     protected void controlUpdate() throws ExceptionServiceLog {
-        
+        controlCreate();
     }
 
     @Override
