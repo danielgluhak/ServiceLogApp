dbb54af3e34887dc6c5be17efa69e48a8bb25096
MojProfil <gluhakdaniel@gmail.com>
2021-04-15 23:50:48 +0200

    Deprecated

--- src/main/java/com/danielgluhak/view/SplashScreen.java
+++ src/main/java/com/danielgluhak/view/SplashScreen.java
@@ -1,25 +1,34 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.danielgluhak.view;
 
 import com.danielgluhak.util.HibernateUtil;
 import javax.swing.JOptionPane;
 import org.hibernate.Session;
 
+/**
+ *
+ * @author Daniel
+ */
 public class SplashScreen extends javax.swing.JFrame {
 
+    /**
+     * Creates new form SplshScreen
+     */
     public SplashScreen() {
         initComponents();
         new Loader().start();
-        
-   
-        
     }
+    
     public void startSplashScreen() {
         try{
         for(int p=0; p<=100; p++){
             Thread.sleep(20);
             splscrProgressBar.setValue(p);
-            splscrLoadingLabel.setText("Loading..."+(Integer.toString(p)+"%"));
+//            txtProgress.setText("Loading..."+(Integer.toString(p)+"%"));
             if(p==100){
                 splscrProgressBar.setVisible(false);
                     }
@@ -27,8 +36,7 @@ public class SplashScreen extends javax.swing.JFrame {
             }catch(Exception e){
         }
     }
-    
-     private class Loader extends Thread {
+    private class Loader extends Thread {
         
          @Override
          public void run() {
@@ -41,52 +49,33 @@ public class SplashScreen extends javax.swing.JFrame {
             }
         }
     }
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        splscrLoadingLabel = new javax.swing.JLabel();
+        jLabel = new javax.swing.JLabel();
         splscrProgressBar = new javax.swing.JProgressBar();
-        splscrBackgroundLabel = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setType(java.awt.Window.Type.POPUP);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        splscrLoadingLabel.setFont(new java.awt.Font("SansSerif", 1, 11)); // NOI18N
-        splscrLoadingLabel.setForeground(new java.awt.Color(255, 255, 255));
-        splscrLoadingLabel.setPreferredSize(new java.awt.Dimension(50, 15));
-        getContentPane().add(splscrLoadingLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 290, 120, 20));
+        jLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/istockphoto-957612802-612x612.jpg"))); // NOI18N
+        getContentPane().add(jLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 610, 370));
 
-        splscrProgressBar.setBackground(new java.awt.Color(204, 204, 204));
-        splscrProgressBar.setForeground(new java.awt.Color(51, 255, 0));
-        getContentPane().add(splscrProgressBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 280, 190, -1));
+        splscrProgressBar.setForeground(new java.awt.Color(0, 204, 51));
+        getContentPane().add(splscrProgressBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 330, 190, 20));
 
-        splscrBackgroundLabel.setIcon(new javax.swing.ImageIcon("C:\\NetBeans-workspace\\ServiceLogApp\\src\\main\\resources\\Webp.net-resizeimage.jpg")); // NOI18N
-        splscrBackgroundLabel.setOpaque(true);
-        getContentPane().add(splscrBackgroundLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 338));
-
-        setSize(new java.awt.Dimension(614, 375));
+        pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel splscrBackgroundLabel;
-    private javax.swing.JLabel splscrLoadingLabel;
+    private javax.swing.JLabel jLabel;
     private javax.swing.JProgressBar splscrProgressBar;
     // End of variables declaration//GEN-END:variables
 }
